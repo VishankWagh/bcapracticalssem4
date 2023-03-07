232120cdcf53f2f923e2ce179ee1ac9572979e01
VishankWagh <vishankswagh@gmail.com>
2023-03-07 10:35:11 +0530

    first commit

---
+++ readfile.js
@@ -0,0 +1,10 @@
+let http = require('http');
+let fs = require('fs');
+// let port = 8049;
+http.createServer(function (req, res){
+    fs.readFile('resume.html', function (err, data) {
+        res.setHeader(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(3000);
