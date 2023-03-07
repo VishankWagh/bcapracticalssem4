5f099c9faeace05f5d911b6d5f81c6ecbc6811e7
VishankWagh <vishankswagh@gmail.com>
2023-03-07 10:45:19 +0530

    second commit

--- readfile.js
+++ readfile.js
@@ -3,7 +3,7 @@ let fs = require('fs');
 // let port = 8049;
 http.createServer(function (req, res){
     fs.readFile('resume.html', function (err, data) {
-        res.setHeader(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
     });
